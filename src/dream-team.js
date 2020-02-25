8af32a584d62e058d2510ff6743e81b466cf61f4
Islombek Khasanov <islombekhasanov@gmail.com>
2020-02-25 19:14:40 +0500

    feat: solve Dream Team Task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,8 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  return members.reduce((accumulator, member) => {
+    if ((typeof member) == 'string')
+      accumulator.push(member.trim().toUpperCase()[0])
+    return accumulator
+  }, []).sort((a, b) => a.localeCompare(b)).join('')
 };
