a052416af4dec37c50d038670ba28bdec65530c2
Islombek Khasanov <islombekhasanov@gmail.com>
2020-02-25 18:19:44 +0500

    feat: solve Hanoi Tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let turns = Math.pow(2, disksNumber) - 1
-    let seconds = turns / turnsSpeed * 60 * 60
+    let seconds = turns / (turnsSpeed / 3600)
     return {
         turns: turns,
         seconds: seconds
