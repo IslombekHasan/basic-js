d8c809cdaf2ef3cc4f0004bb9f7178b21f8ed9c1
Islombek Khasanov <islombekhasanov@gmail.com>
2020-02-25 19:51:31 +0500

    feat: solve WhatSeason task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,31 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!'
+  if (Object.prototype.toString.call(date) != '[object Date]') throw Error
+
+  let month = (date.getMonth() + 1).toString()
+  let season = '';
+  switch (month) {
+    case '12':
+    case '1':
+    case '2':
+      season = 'winter';
+      break;
+    case '3':
+    case '4':
+    case '5':
+      season = 'spring';
+      break;
+    case '6':
+    case '7':
+    case '8':
+      season = 'summer';
+      break;
+    case '9':
+    case '10':
+    case '11':
+      season = 'fall';
+      break;
+  }
+
+  return season
 };
