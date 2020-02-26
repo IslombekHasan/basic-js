6d30dfdc4bb2ae95660219769ab1262f4028329d
Islombek Khasanov <islombekhasanov@gmail.com>
2020-02-26 17:30:19 +0500

    feat: solve and finish Transform Array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+let discardNext = "--discard-next"
+let discardPrev = "--discard-prev"
+let doubleNext = "--double-next"
+let doublePrev = "--double-prev"
+
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw Error
+    let accumulator = []
+    for (let index = 0; index < arr.length; index++) {
+        const currentValue = arr[index];
+        switch (currentValue) {
+            case discardNext:
+                if (index < arr.length - 1) index++;
+                break;
+            case discardPrev:
+                if (index > 0) accumulator.pop()
+                break;
+            case doubleNext:
+                if (index < arr.length - 1) accumulator.push(arr[index + 1])
+                break;
+            case doublePrev:
+                if (index != 0) accumulator.push(arr[index - 1])
+                break
+            default:
+                accumulator.push(currentValue)
+                break;
+        }
+    }
+    return accumulator
 };
