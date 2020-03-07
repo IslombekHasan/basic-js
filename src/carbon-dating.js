9ed8811e5566e5298f1bed5c02d8c27e170001ef
Islombek Khasanov <islombekhasanov@gmail.com>
2020-03-07 15:52:52 +0500

    feat: test and finish Carbon Dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity != 'string') return false
+
+  const numberActivity = parseFloat(sampleActivity)
+  if (!(numberActivity && numberActivity > 0 && numberActivity < MODERN_ACTIVITY)) return false;
+
+  let N_N0 = MODERN_ACTIVITY / numberActivity;
+
+  let k = 0.693 / HALF_LIFE_PERIOD
+
+  let t = Math.log(N_N0) / k
+
+  return Math.ceil(t)
 };
